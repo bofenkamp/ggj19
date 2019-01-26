4e095c62d9c6132af52660eaee74c4a603091caa
Matthew Bofenkamp <mbofenka@andrew.cmu.edu>
2019-01-26 15:50:43 -0500

    grandma can only see food in her field of view

--- Global Game Jam 2019/Assets/Scripts/FoodFinder.cs
+++ Global Game Jam 2019/Assets/Scripts/FoodFinder.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class FoodFinder : MonoBehaviour
 {
+    [SerializeField]
+    private float fieldOfView;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,13 +16,22 @@ public class FoodFinder : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(CanSeeFood());
+        CanSeeFood();
     }
 
     public bool CanSeeFood ()
     {
         Transform food = this.transform;
         Transform grandma = GameObject.FindGameObjectWithTag("Eyes").transform;
-        return !Physics.Linecast(food.position, grandma.position);
+        Transform gaze = grandma.GetChild(0);
+        if (Physics.Linecast(food.position, grandma.position)) //is there an object blocking grandma's view?
+            return false;
+        else
+        {
+            Vector3 gazeDir = gaze.position - grandma.position;
+            Vector3 foodDir = food.position - grandma.position;
+            float angle = Vector3.Angle(gazeDir, foodDir);
+            return angle <= fieldOfView;
+        }
     }
 }
