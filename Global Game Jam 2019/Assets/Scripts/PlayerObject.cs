fc6bd09846b7b847fae6c11930bf7a84c3a6a2d3
Aakash Phadtare <iaakash31@gmail.com>
2019-01-26 12:59:02 -0500

    1pm

---
+++ Global Game Jam 2019/Assets/Scripts/PlayerObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerObject : NetworkBehaviour {
+
+    // Use this for initialization
+
+    public GameObject SpherePrefab;
+
+	void Start () {
+        if (!isLocalPlayer)
+            return;
+
+        CmdSpawnMyUnit();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isLocalPlayer)
+            return;
+        
+    }
+    
+
+    [Command]
+    void CmdSpawnMyUnit()
+    {
+        GameObject go = Instantiate(SpherePrefab);
+
+        NetworkServer.SpawnWithClientAuthority(go, connectionToClient);
+    }
+
+
+}
