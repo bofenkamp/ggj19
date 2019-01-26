7aebfb8ca1bf71f12a939f1fc90738460e025b36
Matthew Bofenkamp <mbofenka@andrew.cmu.edu>
2019-01-25 23:47:52 -0500

    airconsole tutorials

---
+++ Global Game Jam 2019/Assets/GameLogic.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+public class GameLogic : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        AirConsole.instance.onMessage += OnMessage;
+    }
+
+    void OnMessage(int fromDeviceID, JToken data)
+    {
+        Debug.Log("message from " + fromDeviceID + ", data: " + data);
+        if (data ["action"] != null && data ["action"].ToString().Equals("interact"))
+        {
+            Camera.main.backgroundColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        }
+    }
+
+    private void OnDestroy()
+    {
+        //unregister events
+        if (AirConsole.instance != null)
+        {
+            AirConsole.instance.onMessage -= OnMessage;
+        }
+    }
+
+}
