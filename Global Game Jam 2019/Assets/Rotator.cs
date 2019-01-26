8b57e54f7023615114dceccdb6824faa1eaa4e38
Matthew Bofenkamp <mbofenka@andrew.cmu.edu>
2019-01-26 11:43:37 -0500

    Turning and throwing work in harmony

---
+++ Global Game Jam 2019/Assets/Rotator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+
+    Gyroscope gyro;
+    Vector3 initRot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gyro = Input.gyro;
+        gyro.enabled = true;
+        initRot = Input.acceleration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.C))
+            Calibrate();
+            
+        Rotate(gyro.rotationRate);
+    }
+
+    public void Rotate(Vector3 rot)
+    {
+        transform.eulerAngles = initRot + new Vector3(-rot.x, -rot.y, rot.z);
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 0);
+        initRot = transform.eulerAngles;
+    }
+
+    public void Calibrate ()
+    {
+        initRot = Vector3.zero;
+    }
+}
---
+++ Global Game Jam 2019/Assets/Scripts/SwipeBall.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeBall : MonoBehaviour {
+
+    Vector2 startPos, endPos, direction;        //Touch start position, end position, direction
+    float touchTimeStart, touchTimeFinish, timeInterval;    //To calculate swipe time to control throw force in Z direction
+
+    [SerializeField]
+    float throwForceInXAndY = 1f;   //To control throw force in X and Y dirs
+
+    [SerializeField]
+    float throwForceInZ = 50f; //To control throw force in Z direction
+
+    Rigidbody rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();	
+	}
+	
+	void Update () {
+		
+        if(Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began)    //When you touch the screen
+        {
+            touchTimeStart = Time.time;
+            startPos = Input.GetTouch(0).position;
+            Debug.Log("I got touched");
+        }
+
+        if(Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Ended)
+        {
+            Vector3 lookDir = GetLookDir();
+
+            touchTimeFinish = Time.time;
+            timeInterval = touchTimeFinish - touchTimeStart;
+
+            endPos = Input.GetTouch(0).position;    //Getting release finger position
+
+            direction = startPos - endPos;          //Calculating swipe direction in 2D space
+
+            rb.isKinematic = false;
+            //Add force to ball's rigidbody in 3D space depending on above variables
+            Vector3 throwDirForce = new Vector3(-direction.x * throwForceInXAndY, -direction.y * throwForceInXAndY, throwForceInZ / timeInterval);
+
+            Vector3 throwForce = (throwForceInZ / timeInterval) * lookDir;
+            throwForce += new Vector3(throwDirForce.x, throwDirForce.y, 0f);
+            Debug.Log(throwForce);
+
+            //rb.AddForce(RotateByAmount(throwDirForce, lookDir));
+            rb.AddForce(throwForce);
+            transform.parent = null;
+        }
+
+	}
+
+    Vector3 GetLookDir()
+    {
+        Transform cam = transform.parent;
+        Transform marker = cam.GetChild(0);
+        return marker.position - cam.position;
+    }
+
+    Vector3 RotateByAmount (Vector3 init, Vector3 rot)
+    {
+        Vector3 result = rot;
+        result = result * init.magnitude / rot.magnitude;
+        result = result + init;
+
+        return result;
+    }
+}
