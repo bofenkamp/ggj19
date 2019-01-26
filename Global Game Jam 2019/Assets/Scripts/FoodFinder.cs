0bc66c7678ed868c2a36adda562836c266f7eab3
Matthew Bofenkamp <mbofenka@andrew.cmu.edu>
2019-01-26 15:26:01 -0500

    can tell if something is between grandma and food

--- Global Game Jam 2019/Assets/Scripts/FoodFinder.cs
+++ Global Game Jam 2019/Assets/Scripts/FoodFinder.cs
@@ -13,13 +13,13 @@ public class FoodFinder : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(CanSeeFood());
     }
 
     public bool CanSeeFood ()
     {
         Transform food = this.transform;
         Transform grandma = GameObject.FindGameObjectWithTag("Eyes").transform;
-        return Physics.Linecast(food.position, grandma.position);
+        return !Physics.Linecast(food.position, grandma.position);
     }
 }
