507ddf0facae7cbf788e051a32555436e2262472
Aakash Phadtare <iaakash31@gmail.com>
2019-01-25 23:07:11 -0500

    Ball Physics

---
+++ Global Game Jam 2019/Assets/Scripts/RespawnBall.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnBall : MonoBehaviour {
+
+    // Use this for initialization
+
+    [SerializeField]
+    GameObject ballPrefab;
+
+    [SerializeField]
+    GameObject ballGameObject;
+
+    Vector3 OriginalPos;
+
+
+    private void Awake()
+    {
+        OriginalPos = ballGameObject.transform.position;
+    }
+
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public void RespawnIt()
+    {
+        
+        Destroy(ballGameObject);
+        ballGameObject = Instantiate(ballPrefab, OriginalPos, Quaternion.identity);
+    }
+}
---
+++ Global Game Jam 2019/Assets/Scripts/SwipeBall.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeBall : MonoBehaviour {
+
+    Vector2 startPos, endPos, direction;        //Touch start position, end position, direction
+    float touchTimeStart, touchTimeFinish, timeInterval;    //To calculate swipe time to control throw force in Z direction
+
+    [SerializeField]
+    float throwForceInXAndY = 1f;   //To control throw force in X and Y dirs
+
+    [SerializeField]
+    float throwForceInZ = 50f; //To control throw force in Z direction
+
+    Rigidbody rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();	
+	}
+	
+	void Update () {
+		
+        if(Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began)    //When you touch the screen
+        {
+            touchTimeStart = Time.time;
+            startPos = Input.GetTouch(0).position;
+            Debug.Log("I got touched");
+        }
+
+        if(Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Ended)
+        {
+
+            touchTimeFinish = Time.time;
+            timeInterval = touchTimeFinish - touchTimeStart;
+
+            endPos = Input.GetTouch(0).position;    //Getting release finger position
+
+            direction = startPos - endPos;          //Calculating swipe direction in 2D space
+
+            rb.isKinematic = false;
+            //Add force to ball's rigidbody in 3D space depending on above variables
+            rb.AddForce(-direction.x * throwForceInXAndY, -direction.y * throwForceInXAndY, throwForceInZ / timeInterval);
+        }
+
+	}
+}
